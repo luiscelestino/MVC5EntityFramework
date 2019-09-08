ddd514ecd6834c630e70f3a7d066b3d7e9a2ecb3
luiscelestino <souza.luis@gmail.com>
2019-09-07 21:28:24 -0300

    Add files via upload

---
+++ WebApplication1/Models/MyContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class MyContext : DbContext, IDisposable
+    {
+        public DbSet<Pessoa> Pessoa { get; set; }
+
+        public MyContext() 
+            : base(ConfigurationManager.ConnectionStrings["conexao"].ConnectionString) { }
+
+    }
+}
